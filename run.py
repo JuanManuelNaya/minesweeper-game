ce0bb2401183cac7774b2a52d69ea8a2bbc976b6
Juan Naya <klanderfil@gmail.com>
2022-06-03 20:31:29 +0000

    fixing issue

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import random
 import re
 
-# lets create a board object to represent the minesweeper game
-# this is so that we can just say "create a new board object", or
-# "dig here", or "render this game for this object"
 class Board:
     def __init__(self, dim_size, num_mines):
         """
@@ -13,9 +10,9 @@ class Board:
         """
         self.dim_size = dim_size
         self.num_mines = num_mines
-        self.board = self.create_new_board() # plant the mines
+        self.board = self.create_new_board() # Plant mines
         self.allocate_values_to_board()
-        self.dug = set() # if we dig at 0, 0, then self.dug = {(0,0)}
+        self.dug = set() # For example if we dig at 0, 0, then self.dug = {(0,0)}
 
     def create_new_board(self):
         """
@@ -23,17 +20,10 @@ class Board:
         """
         # generate a new board
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
-        # this creates an array like this:
-        # [[None, None, ..., None],
-        #  [None, None, ..., None],
-        #  [...                  ],
-        #  [None, None, ..., None]]
-        # we can see how this represents a board!
-
      
         mines_planted = 0
         while mines_planted < self.num_mines:
-            loc = random.randint(0, self.dim_size**2 - 1) # return a random integer N such that a <= N <= b
+            loc = random.randint(0, self.dim_size**2 - 1)  # return a random integer 
             row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us what row to look at
             col = loc % self.dim_size  # we want the remainder to tell us what index in that row to look at
 
@@ -41,7 +31,7 @@ class Board:
                 # this means we've actually planted a mine in that position already, so keep going
                 continue
 
-            board[row][col] = '*' # plant the mine
+            board[row][col] = '*' # Plant the mine
             mines_planted += 1
 
         return board
@@ -53,8 +43,7 @@ class Board:
         """
         for r in range(self.dim_size):
             for c in range(self.dim_size):
-                if self.board[r][c] == '*':
-                    # if this is already a bomb, we don't want to calculate anything
+                if self.board[r][c] == '*':  # if this is already a mine, nothing happens, continue
                     continue
                 self.board[r][c] = self.obtain_num_neighboring_mines(r, c)
 
@@ -76,8 +65,7 @@ class Board:
         num_neighboring_mines = 0
         for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
             for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
-                if r == row and c == col:
-                    # our original location, don't check
+                if r == row and c == col:  # our original location, don't check, continue
                     continue
                 if self.board[r][c] == '*':
                     num_neighboring_mines += 1
@@ -91,11 +79,13 @@ class Board:
         Return False in case of mine dug.
         Different situations:
         hit a mine -> game over
-        # dig at location with neighboring mine -> finish dig
-        # dig at location with no neighboring mine -> recursively dig neighbors!
+        # dig at location with neighboring mine
+         -> finish dig
+        # dig at location with no neighboring mine
+        -> recursively dig neighbors!
 
         """
-        self.dug.add((row, col)) # keep track that we dug here
+        self.dug.add((row, col))  # keep track that we dug here
 
         if self.board[row][col] == '*':
             return False
@@ -106,7 +96,7 @@ class Board:
         for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
             for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
                 if (r, c) in self.dug:
-                    continue # don't dig where you've already dug
+                    continue   # don't dig where you've already dug
                 self.dig(r, c)
 
         # if our initial dig didn't hit a mine, we *shouldn't* hit a mine here
@@ -116,8 +106,7 @@ class Board:
         """
         It will return a string that shows the board to the player.
         """
-      
-        # first let's create a new array that represents what the user would see
+        # Create a new array that represents what the user would see
         visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
         for row in range(self.dim_size):
             for col in range(self.dim_size):
@@ -126,9 +115,9 @@ class Board:
                 else:
                     visible_board[row][col] = ' '
         
-        # put this together in a string
-        string_rep = ''
-        # get max column widths for printing
+        # Putting this together in a string
+        string_representation = ''
+        # Getting max column widths for printing
         widths = []
         for idx in range(self.dim_size):
             columns = map(lambda x: x[idx], visible_board)
@@ -150,18 +139,18 @@ class Board:
         
         for i in range(len(visible_board)):
             row = visible_board[i]
-            string_rep += f'{i} |'
+            string_representation += f'{i} |'
             cells = []
             for idx, col in enumerate(row):
                 format = '%-' + str(widths[idx]) + "s"
                 cells.append(format % (col))
-            string_rep += ' |'.join(cells)
-            string_rep += ' |\n'
+            string_representation += ' |'.join(cells)
+            string_representation += ' |\n'
 
-        str_len = int(len(string_rep) / self.dim_size)
-        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+        str_len = int(len(string_representation) / self.dim_size)
+        string_representation = indices_row + '-'*str_len + '\n' + string_representation + '-'*str_len
 
-        return string_rep
+        return string_representation
 
 # play the game
 def play(dim_size=10, num_mines=10):
@@ -170,16 +159,17 @@ def play(dim_size=10, num_mines=10):
     1- Create the board and plant the mines.
     2- Show the user the board and ask for position to dig.
     3a- If location is a mine, show Game Over message.
-    3b- If location is not a mine, dig recursively until each square is at least next to a mine
-    4- Repeats steps 2 and 3a/b until there are no more plances to dig -> This is a Victory
+    3b- If location is not a mine, 
+    dig recursively until each square is at least next to a mine
+    4- Repeats steps 2 and 3a/b until there are no more plances to dig 
+    -> This is a Victory
 
     """
     board = Board(dim_size, num_mines)
     secure = True 
     while len(board.dug) < board.dim_size ** 2 - num_mines:
         print(board)
-        # 0,0 or 0, 0 or 0,    0
-        user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row,col: "))  # '0, 3'
+        user_input = re.split(',(\\s)*', input("Time to dig. Input as row,col: "))  # 0,0 or 0, 0 or 0,    0
         row, col = int(user_input[0]), int(user_input[-1])
         if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
             print("Invalid location. Try again.")
@@ -196,9 +186,9 @@ def play(dim_size=10, num_mines=10):
         print("No more plances to dig -> You Won the Game!")
     else:
         print("You dug a mine! -> GAME OVER")
-        # let's reveal the whole board!
+        # Reveal the whole board!
         board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
         print(board)
 
-if __name__ == '__main__': # good practice :)
+if __name__ == '__main__': # good practice
     play()
